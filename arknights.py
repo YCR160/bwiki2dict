d115e40767a2a95d3aaf3ca466a61690448521bf
CR Ye <3342711246@qq.com>
2024-10-18 21:21:20 +0800

    add: 添加一些游戏

---
+++ arknights.py
@@ -0,0 +1,6 @@
+import config
+
+config.exports["source"]["api_path"] = "https://wiki.biligame.com/arknights/api.php"
+config.exports["generator"][0]["kwargs"]["output"] = "arknights.dict.yml"
+
+exports = config.exports
---
+++ sr.py
@@ -0,0 +1,6 @@
+import config
+
+config.exports["source"]["api_path"] = "https://wiki.biligame.com/sr/api.php"
+config.exports["generator"][0]["kwargs"]["output"] = "sr.dict.yml"
+
+exports = config.exports
