55b08775098ef13509709e312e356e0618e3ffad
CR Ye <3342711246@qq.com>
2024-10-24 11:15:23 +0800

    add: 筛选特定分类

---
+++ fetch.py
@@ -0,0 +1,54 @@
+import urllib3
+from deepmerge import always_merger
+
+http = urllib3.PoolManager()
+
+gaplimit = 500
+cllimit = 500
+
+HEADERS = {
+    "User-Agent": f"BWIKI2Dict/0.1.0; github.com/YCR160/bwiki2dict",
+    "Accept-Encoding": "gzip, deflate"
+}
+
+def fetch_all_titles(api_url = "https://wiki.biligame.com/sr/api.php"):
+    data = {}
+    all_data = {}
+
+    gapcontinue = None
+    clcontinue = None
+
+    fetch_url = f"{api_url}?action=query&generator=allpages&prop=categories&format=json&gaplimit={gaplimit}&cllimit={cllimit}"
+
+    cont = True
+    while cont:
+        clcontinue = data.get("continue", {}).get("clcontinue")
+
+        if 'gapcontinue' in data.get("continue", {}).get("continue", {}):
+            gapcontinue = data.get("continue", {}).get("gapcontinue")
+
+        # print(f"gapcontinue: {gapcontinue}, clcontinue: {clcontinue}")
+
+        now_fetch_url = fetch_url
+        if gapcontinue:
+            now_fetch_url += f"&gapcontinue={gapcontinue}"
+        if clcontinue:
+            now_fetch_url += f"&clcontinue={clcontinue}"
+
+        # print(now_fetch_url)
+
+        resp = http.request("GET", now_fetch_url, headers=HEADERS, retries=3)
+        data = resp.json()
+
+        all_data = always_merger.merge(all_data, data)
+
+        cont = data.get("continue", False)
+
+    return all_data
+
+if __name__ == "__main__":
+    import json
+
+    all_data = fetch_all_titles()
+    with open("allpages.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(all_data, ensure_ascii=False, indent=2))
---
+++ filter.py
@@ -0,0 +1,45 @@
+def filter_titles(all_data, part_categories):
+    all_pages = all_data.get("query", {}).get("pages", {})
+
+    # 构建 分类: [标题1, 标题2, ...] 的字典
+    category_to_pages = {}
+    for _, page in all_pages.items():
+        categories = page.get("categories", [])
+        for category in categories:
+            category_title = category.get("title", "")
+            if category_title not in category_to_pages:
+                category_to_pages[category_title] = []
+            category_to_pages[category_title].append(page.get("title", ""))
+
+    # 构建 部分分类: [标题1, 标题2, ...] 的字典
+    part_category_to_pages = {}
+    for category in part_categories:
+        part_category_to_pages[category] = category_to_pages.get(category, [])
+
+    # 部分标题
+    part_titles = set()
+    for category, pages in part_category_to_pages.items():
+        for page in pages:
+            part_titles.add(page)
+
+    return part_titles
+
+if __name__ == "__main__":
+    from fetch import fetch_all_titles
+
+    api_url = "https://wiki.biligame.com/sr/api.php"
+    all_data = fetch_all_titles(api_url)
+
+    all_pages = all_data.get("query", {}).get("pages", {})
+    # print(all_pages)
+    all_categories = set()
+    for page_id, page in all_pages.items():
+        # print(page)
+        categories = page.get("categories", [])
+        # print(categories)
+        for category in categories:
+            category_title = category.get("title", "")
+            # print(category_title)
+            all_categories.add(category_title)
+
+    print(all_categories)
