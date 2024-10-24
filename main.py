92901c9fe08ffbe1edde23b41d7d7f0b07822fc7
CR Ye <3342711246@qq.com>
2024-10-24 18:17:25 +0800

    add: 批量生成词典

---
+++ main.py
@@ -0,0 +1,19 @@
+from fetch import fetch_all_titles
+from filter import filter_titles
+from tweaks.bwiki import tweaks
+from convert import export
+from dictgen import *
+
+from config import configs
+
+for game, data in configs.items():
+    api_url = data["api_url"]
+    part_categories = data["part_categories"]
+
+    all_data = fetch_all_titles(api_url)
+    filtered_titles = filter_titles(all_data, part_categories)
+    for func in tweaks:
+        filtered_titles = func(filtered_titles)
+    pinyin_titles = export(filtered_titles)
+
+    rime(pinyin_titles, name=game)
