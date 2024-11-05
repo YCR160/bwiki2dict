3983d475e6af4a816792199194cfa6c4694cef75
CR Ye <3342711246@qq.com>
2024-11-05 14:38:01 +0800

    refactor: 更换明日方舟 API

--- config/arknights.py
+++ config/arknights.py
@@ -1,3 +1,4 @@
-api_url = "https://wiki.biligame.com/arknights/api.php"
+# api_url = "https://wiki.biligame.com/arknights/api.php"
+api_url = "https://prts.wiki/api.php"
 
 part_categories = {"分类:干员"}
